294f6b80f4f49b096244e4d248cfe92f752f2f37
Viktor Pazhev <asusvikp@gmail.com>
2021-05-30 17:44:44 +0300

    first prototype

---
+++ Jungle Speed/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jungle_Speed {
+    class Program {
+        static void PrintTable(string[][] table) {
+            foreach (string[] sym in table) {
+                Array.ForEach(sym, Console.Write);
+                Console.Write('\n');
+            }
+        }
+        static void Main(string[] args) {
+            bool inGame = true;
+            Console.OutputEncoding = Encoding.UTF8;
+            string a = "\u263B";
+            string[][] table = new string[11][];
+            for (int i=0; i<11; i++) {
+                table[i] = new string[21];
+                for (int j = 0; j < 21; j++) {
+                    table[i][j] = a;
+                }
+            }
+            while(inGame) {
+                Console.Clear();
+                PrintTable(table);
+                Console.ReadLine();
+            }
+        }
+    }
+}
