46406713b279f49e67277c2a01cb8ec5c021c4e9
bonevboyan <boyanbonev43@gmail.com>
2021-05-30 18:34:12 +0300

    Create Totem.cs

---
+++ JungleSpeed/JungleSpeed/Models/Totem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleSpeed.Models
+{
+    class Totem : GameObject
+    {
+        public Totem(IRenderer renderer)
+            : base(renderer)
+        {
+        }
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
