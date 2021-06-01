5023d9d581af929f12881ed305d73ade3be447a2
Eko199 <50295282+Eko199@users.noreply.github.com>
2021-06-01 14:05:55 +0300

    Deck and played cards to Player

--- JungleSpeed/JungleSpeed/Models/Player.cs
+++ JungleSpeed/JungleSpeed/Models/Player.cs
@@ -7,13 +7,15 @@ namespace JungleSpeed.Models
 {
     class Player : GameObject
     {
-        private Queue<Card> cards;
+        private Queue<Card> deck;
+        private Queue<Card> playedCards;
         public Player(IRenderer renderer)
             : base(renderer)
         {
         }
         public override void Draw()
         {
+            playedCards.Seek().DrawAtPosition();
             throw new NotImplementedException();
         }
     }
